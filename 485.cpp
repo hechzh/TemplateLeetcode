99fdc45b8d469d89eb09ab8212c4b11d406950d5
hechzh <hechzh@shanghaitech.edu.cn>
2021-02-15 13:13:50 +0800

    leetcode485

---
+++ 485.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+#define TEST_CASE_485 1, 1, 0, 1, 1 ,1
+
+using namespace std;
+
+template <int... Ints>
+using int_sequence = std::integer_sequence<int, Ints...>;
+
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int answer = 0;
+        int temp_answer = 0;
+        for (auto i : nums)
+        {
+            if (i == 0)
+            {
+                if (temp_answer)
+                {
+                    answer = answer > temp_answer ? answer : temp_answer;
+                    temp_answer = 0;
+                }
+            }
+            else
+            {
+                temp_answer++;
+            }
+        }
+        answer = answer > temp_answer ? answer : temp_answer;
+        return answer;
+    }
+};
+
+template <int max, int counter>
+int findMaxConsecutiveOnes(int_sequence<>) { return 0; }
+
+template <int max, int counter, int Itop, int... I>
+int findMaxConsecutiveOnes(int_sequence<Itop, I...>)
+{
+    if constexpr (sizeof...(I) == 0)
+    {
+        if constexpr (Itop == 1)
+            return max > counter + 1 ? max : counter + 1;
+        else
+            return max > counter ? max : counter;
+    }
+    if constexpr (Itop == 0)
+        return findMaxConsecutiveOnes< (max > counter ? max : counter), 0 > (int_sequence<I...>());
+    else
+        return findMaxConsecutiveOnes<max, counter + 1>(int_sequence<I...>());
+}
+
+template <int Itop, int... I>
+int findMaxConsecutiveOnes(int_sequence<Itop, I...> _)
+{
+    return findMaxConsecutiveOnes<0, 0>(_);
+}
+
+int main()
+{
+    Solution s;
+    vector<int> test = {TEST_CASE_485};
+    int result = s.findMaxConsecutiveOnes(test);
+    cout << result << endl;
+    auto template_test = int_sequence<TEST_CASE_485>();
+    int template_result = findMaxConsecutiveOnes(template_test);
+    cout<<template_result<<endl;
+}
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## template programming for leetcode problems
 
+119.cpp add a basic example for how meta programming work and transfer integer_sequence to array
 449.cpp add some functions to do similar operations as vector does
+485.cpp a toy example for functional programming reducing a list
+765.cpp reduce operation and find and swap operation
