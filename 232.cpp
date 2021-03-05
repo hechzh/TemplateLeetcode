df1233561fb4b385fffe747caa99a2f9005e8bbd
hechzh <hechzh@shanghaitech.edu.cn>
2021-03-05 13:48:06 +0800

    leetcode232

---
+++ 232.cpp
@@ -0,0 +1,53 @@
+
+class MyQueue {
+public:
+    stack<int> instack;
+    stack<int> outstack;
+    /** Initialize your data structure here. */
+    MyQueue():instack(),outstack() {
+
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        instack.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if(outstack.empty()){
+            while(!instack.empty()){
+                outstack.push(instack.top());
+                instack.pop();
+            }
+        }
+        int Answer=outstack.top();
+        outstack.pop();
+        return Answer;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if(outstack.empty()){
+            while(!instack.empty()){
+                outstack.push(instack.top());
+                instack.pop();
+            }
+        }
+        return outstack.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return instack.empty() && outstack.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
