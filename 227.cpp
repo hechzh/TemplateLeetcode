91c9402291f95a73c2970e2a9efa3fdc47c8cf26
hechzh <hechzh@shanghaitech.edu.cn>
2021-03-11 22:02:12 +0800

    leetcode227

---
+++ 227.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calculate(string s) {
+        vector<int> stk;
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n;i++) {
+            while (isdigit(s[i])) {
+                num = num * 10 + int(s[i] - '0');
+                i++;
+            }
+            if (!isdigit(s[i]) && s[i] != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stk.push_back(num);
+                        break;
+                    case '-':
+                        stk.push_back(-num);
+                        break;
+                    case '*':
+                        stk.back() *= num;
+                        break;
+                    default:
+                        stk.back() /= num;
+                }
+                preSign = s[i];
+                num = 0;
+            }
+        }
+        return accumulate(stk.begin(), stk.end(), 0);
+    }
+};
